f06a1d3261f1df286f27aba8f167847a5b20c4fe
Jacob Meleka <melekajacob@yahoo.ca>
2021-05-09 21:37:12 -0400

    Finished initial frontend component structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import AddQuestionButton from "./components/AddQuestionButton";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Container>
+        <Row className="m-3">
+          <Col className="col-md-12 text-center">
+            <AddQuestionButton />
+          </Col>
+        </Row>
+      </Container>
+      <hr></hr>
+    </Container>
   );
 }
 
---
+++ src/components/AddQuestionButton.js
@@ -0,0 +1,31 @@
+import { Button } from "react-bootstrap";
+import { useState } from "react";
+import { FaPlusCircle } from "react-icons/fa";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import AddQuestionModal from "./AddQuestionModal";
+
+export default function AddQuestionButton() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleOpen = () => setShow(true);
+
+  return (
+    <div>
+      <Button
+        onClick={handleOpen}
+        className="px-5"
+        variant="outline-success"
+        size="lg"
+        block
+      >
+        <span>
+          <FaPlusCircle /> Add Question
+        </span>
+      </Button>
+
+      <AddQuestionModal show={show} handleClose={handleClose} />
+    </div>
+  );
+}
